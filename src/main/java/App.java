c4ce8796159f587ea3d3beb057bc4cb43d7b4d43
marcklinux@gmail.com <marcklinux@gmail.com>
2024-09-30 21:14:04 -0600

    se modifico el metodo invertir string usando funcionalidades de java 5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,14 @@
 public class App {
 
     private void m1InvertedString(String text){
-        String[] array = text.split("");
+       /* String[] array = text.split("");
         StringBuilder newText = new StringBuilder();
 
         for(int i = array.length; i>0 ; i--){
             newText.append(array[i-1]);
         }
+        System.out.println(newText);*/
+        String newText = new StringBuilder(text).reverse().toString();
         System.out.println(newText);
     }
     public static void main(String[] args) {
