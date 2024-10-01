9ef43f5ad6917b9a7741afcbe21e467d772d552a
marcklinux@gmail.com <marcklinux@gmail.com>
2024-09-30 21:09:38 -0600

    se agrego el primer metodo para invertir un string

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,16 @@
 public class App {
 
-    public static void main(String[] args) {
+    private void m1InvertedString(String text){
+        String[] array = text.split("");
+        StringBuilder newText = new StringBuilder();
 
+        for(int i = array.length; i>0 ; i--){
+            newText.append(array[i-1]);
+        }
+        System.out.println(newText);
+    }
+    public static void main(String[] args) {
+        App app = new App();
+        app.m1InvertedString("marcos");
     }
 }
